d554381e6a23d28fb3104b6e5e5f400cc1dba200
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-09-08 12:03:43 +0900

    create simple todo app

--- react-lesson/src/App.js
+++ react-lesson/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState, useRef } from 'react';
 import './App.css';
+import TodoList from './TodoList';
+import {v4 as uuidv4} from 'uuid';
+
 
 function App() {
+  const [todos, setTodos] = useState([
+  ]);
+
+  const todoNameRef = useRef();
+
+  // タスクを追加する
+  const handleAddTodo = () => {
+    const name = todoNameRef.current.value;
+    if(name === '') return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, {id: uuidv4(), name: name, completed: false}];
+    });
+    todoNameRef.current.value = null;
+  };
+
+  // タクスの完了状態の変更
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  }
+
+  // タスクの削除
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" ref={todoNameRef}/>
+      <button onClick={handleAddTodo}>タクスを追加</button>
+      <button onClick={handleClear}>完了したタクスを削除</button>
+      <div>残りのタスク：{todos.filter((todo) => !todo.completed).length}</div>
     </div>
   );
 }
